13243bd29bb0f0a00ec414ed3cd0331301f7c04a
Ty Facey <justfacey@gmail.com>
2020-10-15 06:20:33 -0400

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+
+# This is so that you don't have to hardwire the long disription of the
+# package, you can just 'read' it in.
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+setup(
+    name='pgbackup',
+    version='0.1.0',
+    author='Ty Facey',
+    author_email='faceyt@wit.edu',
+    description='A utility for backing up PostreSQL datasets',
+    long_description=long_description,
+    url='https://github.com/faceyacc/pgbackup',
+    # This is where find the sub-packages that is in the 'src' directory
+    packages=find_packages('src')
+)
